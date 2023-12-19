b3d62c8f3ed61dea7c529e18d4bb235dacba9012
ndbzika <chakcpps@gmail.com>
2023-12-18 22:41:21 -0300

    feat: Completando desafio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone: Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(
+            string numero, 
+            string modelo, 
+            string imei, 
+            int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            if (!isTurnedOn)
+            {
+                Console.WriteLine("O celular está desligado, primeiro ligue-o!");
+                return;
+            }
+            Console.WriteLine($"Instalando {nomeApp} na AppStore...");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,21 +3,37 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        protected bool isTurnedOn = false;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
+            if (isTurnedOn)
+            {
+                Console.WriteLine("O celular já está ligado!");
+                return;
+            }
+            isTurnedOn = true;
             Console.WriteLine("Ligando...");
         }
 
         public void ReceberLigacao()
         {
+            if (!isTurnedOn)
+            {
+                Console.WriteLine("O celular está desligado, primeiro ligue-o!");
+                return;
+            }
             Console.WriteLine("Recebendo ligação...");
         }
 
